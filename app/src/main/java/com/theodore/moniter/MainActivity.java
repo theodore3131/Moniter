5ee8b5ae9c8682a418a7fcea9aeb0f224d4ed6eb
theodore3131 <alantheodore32@gmail.com>
2018-06-13 00:32:59 +0800

    Initialization

--- app/src/main/java/com/theodore/moniter/MainActivity.java
+++ app/src/main/java/com/theodore/moniter/MainActivity.java
@@ -1,13 +1,80 @@
 package com.theodore.moniter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private AlertDialog alert = null;
+    private AlertDialog.Builder builder = null;
+    private Context mContext;
+    private boolean[] checkItems;
+    private View view_custom;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = MainActivity.this;
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        builder = new AlertDialog.Builder(mContext);
+
+        final LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+        view_custom = inflater.inflate(R.layout.activity_dialog, null,false);
+
+        builder.setView(view_custom);
+        builder.setCancelable(false);
+        alert = builder.create();
+
+//        view_custom.findViewById(R.id.btn_cancle).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                alert.dismiss();
+//            }
+//        });
+
+        view_custom.findViewById(R.id.btn_blog).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText=view_custom.findViewById(R.id.dialog);
+                Toast.makeText(getApplicationContext(), "绑定成功"+editText.getText().toString(), Toast.LENGTH_SHORT).show();
+                alert.dismiss();
+            }
+        });
+
+        view_custom.findViewById(R.id.btn_close).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "对话框已关闭~", Toast.LENGTH_SHORT).show();
+                alert.dismiss();
+            }
+        });
+
+
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alert.show();
+                //显示对话框
+            }
+        });
     }
+
 }
